f41e2991610e684de9801e517e728738a335e686
Tamim <tamimhossain031@gmail.com>
2024-07-16 00:27:12 +0600

    add slider

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,21 @@ const addHover = () =>{
      )
 }
 window.onload = addHover();
+
+// slider
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        center: true,
+        items:2,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true,
+        margin:20,
+        responsive:{
+            600:{
+                items:3
+            }
+        }
+    });
+  });
